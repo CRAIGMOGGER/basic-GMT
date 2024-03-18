122d6477859eaa6cad269109e28451bc629f3412
Mjcapunitan <mjbcapunitan@gmail.com>
2024-03-18 10:58:57 +0800

    GOMISSION90%

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ burgir.addEventListener("click", () =>{
     burgir.classList.toggle("open");
     hNav.classList.toggle("open");
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.header');
+  
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 100) {
+        header.classList.add('header-scrolled');
+      } else {
+        header.classList.remove('header-scrolled');
+      }
+    });
+  });
